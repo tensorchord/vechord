8b20e88c246a05b1c0d693c216f55543b5ce71b2
Keming <kemingyang@tensorchord.ai>
2025-06-17 12:34:28 +0800

    feat: use gemini API to extract the PDF directly
    Signed-off-by: Keming <kemingyang@tensorchord.ai>

--- vechord/extract.py
+++ vechord/extract.py
@@ -3,8 +3,8 @@ import os
 import unicodedata
 from abc import ABC, abstractmethod
 from html.parser import HTMLParser
-from io import BytesIO
 
+import httpx
 import pypdfium2 as pdfium
 
 from vechord.log import logger
@@ -90,39 +90,54 @@ class SimpleExtractor(BaseExtractor):
 class GeminiExtractor(SimpleExtractor):
     """Extract text with Gemini model."""
 
-    def __init__(self, model: str = "gemini-2.0-flash"):
-        key = os.environ.get("GEMINI_API_KEY")
-        if not key:
+    def __init__(self, model: str = "gemini-2.5-flash-preview-05-20"):
+        self.api_key = os.environ.get("GEMINI_API_KEY")
+        if not self.api_key:
             raise ValueError("env GEMINI_API_KEY not set")
 
-        import google.generativeai as genai
-
-        self.model = genai.GenerativeModel(model)
+        self.model = model
+        self.url = (
+            "https://generativelanguage.googleapis.com/v1beta/models/"
+            f"{self.model}:generateContent"
+        )
         self.prompt = (
-            "Extract all the text from the following document and return it exactly as"
-            " it appears, without any modifications, summarization, or interpretation"
+            "Extract the main content from the PDF document. Ensure to exclude any "
+            "metadata, headers, footers, or any other non-essential information. "
+            "Return the extracted content as it appears in the document, without "
+            "any additional modification, summarization or interpretation."
+        )
+        self.session = httpx.Client(
+            timeout=httpx.Timeout(10.0, read=120.0),
+            headers={"Content-Type": "application/json"},
         )
 
     def name(self) -> str:
-        return f"gemini_extractor_{self.model.model_name}"
+        return f"gemini_extractor_{self.model}"
 
     def extract_pdf(self, doc: bytes) -> str:
         """Extract text from PDF page by page."""
-        pdf = pdfium.PdfDocument(doc)
-        text = []
-        for page in pdf:
-            img = page.render(scale=2).to_pil()  # make the text clearer
-            img_bytes = BytesIO()
-            img.save(img_bytes, format="JPEG")
-            response = self.model.generate_content(
-                [
-                    {
-                        "mime_type": "image/jpeg",
-                        "data": base64.b64encode(img_bytes.getvalue()).decode("utf-8"),
-                    },
-                    self.prompt,
-                ]
-            )
-            text.append(response.text)
+        content = base64.b64encode(doc).decode("utf-8")
+        response = self.session.post(
+            self.url,
+            params={"key": self.api_key},
+            json={
+                "contents": {
+                    "parts": [
+                        {"text": self.prompt},
+                        {
+                            "inline_data": {
+                                "mime_type": "application/pdf",
+                                "data": content,
+                            }
+                        },
+                    ]
+                }
+            },
+        )
 
-        return "\n".join(text)
+        if response.is_error:
+            raise RuntimeError(f"Failed to extract PDF with Gemini: {response.text}")
+
+        data = response.json()
+        text = data["candidates"][0]["content"]["parts"][0]["text"]
+        return text.strip()
